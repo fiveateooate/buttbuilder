b7e03e3d25e2283740705aba4e46576b8824f39e
fiveateooate <fiveateooate@users.noreply.github.com>
2017-11-20 12:24:59 -0700

    delete commented code, change arg names

--- buttbuilder.py
+++ buttbuilder.py
@@ -27,6 +27,7 @@ def node(args, cluster_config_info, builder):
     if args.action == 'remove' and args.name is not None:
         builder.remove_node(args.name)
 
+
 def firewall(args, cluster_config_info, builder):
     if args.action == 'add':
         ports = args.ports.split(",")
@@ -53,62 +54,49 @@ def network(args, cluster_config_info, builder):
         builder.deleteNetwork()
 
 
+def verify(args, cluster_config_info, builder):
+    print("Verifying butt state")
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='make clusters in butts')
     # required args to identify cluster
-    parser.add_argument(
-        '--cluster_env', '-e', help="environment name", required=True)
-    parser.add_argument(
-        '--cluster_id', '-i', help="cluster identifier", required=True)
+    parser.add_argument('--cenv', help="environment name", required=True)
+    parser.add_argument('--cid', help="cluster identifier", required=True)
     # output modifiers
     parser.add_argument('--dryrun', '-d', help="dry run", action='store_true')
     parser.add_argument('--verbose', '-v', help="verbose", action='store_true')
-    parser.add_argument(
-        '--cluster_config_path',
-        help="path to cluster_config file if github is not working",
-        default=None)
+    # base config
+    parser.add_argument('--config', '-c', help="path to cluster_config/yaml file if github is not working", default=None)
     parser.add_argument('--buttdir', help="directory to store cluster data")
     # operations
-    subparsers = parser.add_subparsers(
-        title="buttbuilder operations", help='do things with butts')
+    subparsers = parser.add_subparsers(title="buttbuilder operations", help='do things with butts')
     parser_build = subparsers.add_parser('build', help='Build a butt')
-    #parser_build.add_argument('--createcluster', help='build cluster',action="store_true")
+    # parser_build.add_argument('--createcluster', help='build cluster',action="store_true")
     parser_build.set_defaults(func=build)
 
     parser_node = subparsers.add_parser('node', help='Manage butt nodes')
-    parser_node.add_argument(
-        '--action', help='node actions', choices=["add", "remove"])
+    parser_node.add_argument('--action', help='node actions', choices=["add", "remove"])
     parser_node.add_argument('--name', help='node name to remove')
     parser_node.add_argument("--count", help="nodes to add", default=1, type=int)
     parser_node.set_defaults(func=node)
 
-    parser_firewall = subparsers.add_parser(
-        'firewall', help='Manage butt firewall')
-    parser_firewall.add_argument(
-        '--action',
-        help='firewall rule actions',
-        choices=["add", "remove"],
-        required=True)
+    parser_firewall = subparsers.add_parser('firewall', help='Manage butt firewall')
+    parser_firewall.add_argument('--action', help='firewall rule actions', choices=["add", "remove"], required=True)
     parser_firewall.add_argument('--proto', help='tcp,udp', required=True)
-    parser_firewall.add_argument(
-        '--ports', help='comma separated list of ports', required=True)
-    parser_firewall.add_argument(
-        '--sourcerange', help='rource range', default="0.0.0.0/0")
+    parser_firewall.add_argument('--ports', help='comma separated list of ports', required=True)
+    parser_firewall.add_argument('--sourcerange', help='rource range', default="0.0.0.0/0")
     parser_firewall.set_defaults(func=firewall)
 
-    parser_loadbalancer = subparsers.add_parser(
-        'loadbalancer', help='Manage butt loadbalancers')
-    parser_loadbalancer.add_argument(
-        '--action', help='loadbalancer actions', choices=["add", "remove"])
+    parser_loadbalancer = subparsers.add_parser('loadbalancer', help='Manage butt loadbalancers')
+    parser_loadbalancer.add_argument('--action', help='loadbalancer actions', choices=["add", "remove"])
     parser_loadbalancer.set_defaults(func=loadbalancer)
 
-    parser_network = subparsers.add_parser(
-        'network', help='Manage butt networks')
-    parser_network.add_argument(
-        '--action', help='network actions', choices=["add", "remove", "list"])
+    parser_network = subparsers.add_parser('network', help='Manage butt networks')
+    parser_network.add_argument('--action', help='network actions', choices=["add", "remove", "list"])
     parser_network.set_defaults(func=network)
-
+    parser_verify = subparsers.add_parser('verify', help='Verify butt state')
+    parser_verify.set_defaults(func=verify)
     args = parser.parse_args()
 
     if not hasattr(args, 'func'):
@@ -117,7 +105,7 @@ if __name__ == '__main__':
 
     os.environ['provider'] = ""
     import buttlib.helpers
-    cluster_config_info = buttlib.helpers.ClusterConfigHandler(args.cluster_env, args.cluster_id, args.cluster_config_path)
+    cluster_config_info = buttlib.helpers.ClusterConfigHandler(args.cenv, args.cid, args.config)
 
     os.environ['provider'] = cluster_config_info.env_info['provider']
     import buttlib
--- buttlib/common/builder.py
+++ buttlib/common/builder.py
@@ -2,18 +2,8 @@
     implements common functionallity
 """
 
-# import glob
-# import ipaddress
 import os
-# import re
-# import random
-# import shutil
-# import string
 import subprocess
-# import hashlib
-
-from sh import kubectl
-
 import buttlib
 
 
@@ -30,7 +20,7 @@ class ButtBuilder(object):
         __subnet_mask = env_info['network']['subnetMask'] if 'network' in env_info and 'subnetMask' in env_info['network'] else 24
         __subnet_offset = env_info['network']['subnetOffset'] if 'network' in env_info and 'subnetOffset' in env_info['network'] else 0
         # used in a couple places in __init__
-        __cluster_name = "{}-{}".format(args.cluster_env, args.cluster_id)
+        __cluster_name = "{}-{}".format(args.cenv, args.cid)
         # offset begining master ips default is 10 to allow for dns, k8s cluster ip, ...
         __master_ip_offset = env_info['masters']['ipOffset'] if 'masters' in env_info and 'ipOffset' in env_info['masters'] else 10
         # offset for beginning worker ips, leave some room for masters
@@ -65,8 +55,8 @@ class ButtBuilder(object):
         )
         # huge dict for replacement in ignition files -- created from env_info settings
         self._cluster_info = {
-            "cluster_env": args.cluster_env,
-            "cluster_id": args.cluster_id,
+            "cluster_env": args.cenv,
+            "cluster_id": args.cid,
             "cluster_name": __cluster_name,
             "dns_ip": self._cluster_internal_ips.get_ip(5),
             "master_ip": self._butt_ips.get_ip(__master_ip_offset),  # set master to first master - used to set a lb master if applicable
@@ -74,7 +64,7 @@ class ButtBuilder(object):
             "master_port": 443,  # do we ever want anything else?
             "cluster_ip": self._cluster_internal_ips.get_ip(1),  # used in ssl certs
             "ssh_pub_keys": __ssh_pub_key_helper.get_pub_keys(),
-            "dashboardFQDN": "dashboard-{}.example.com".format(args.cluster_id),
+            "dashboardFQDN": "dashboard-{}.example.com".format(args.cid),
             "etcdVersion": "3.2.9",
             "etcd_hosts": self._kube_masters.etcd_hosts_string,
             "etcd_initial_cluster": self._kube_masters.etcd_initial_cluster_string,
@@ -111,147 +101,3 @@ class ButtBuilder(object):
     def check_cluster(self):
         # verify cluster state ????
         pass
-
-    # @staticmethod
-    # def __read_ud_template(ud_type):
-    #     try:
-    #         with open("butt-templates/{}/user_data_tmpl.yaml".format(ud_type), 'r') as file:
-    #             return file.read()
-    #     except IOError as exc:
-    #         print(exc)
-
-    # def get_kube_masters(self):
-    #     """:returns: list - k8s api endpoints"""
-    #     return ','.join(
-    #         ["https://%s" % (master) for master in self.get_master_hosts()])
-    #
-    # def get_etcd_endpoints(self):
-    #     """:returns: list - etcd endpoints without http:// fo ruse by gateway"""
-    #     return ','.join(
-    #         ["%s:2379" % (master) for master in self.get_master_hosts()])
-    #
-    # def get_master_hosts(self):
-    #     """:returns: list - master host names"""
-    #     return [
-    #         "kube-master-%s-%02d" % (self._cluster_info['cluster_name'], i + 1)
-    #         for i in range(self._env_info['masters']['nodes'])
-    #     ]
-    #
-    # def get_worker_hosts(self):
-    #     """DEPRICATED - convert to non-deterministic hostnames
-    #     :returns: list of worker hostsnames"""
-    #     return [
-    #         "kube-worker-%s-%02d" % (self._cluster_info['cluster_name'], i + 1)
-    #         for i in range(self._env_info['workers']['nodes'])
-    #     ]
-    #
-    # def get_master_ips(self):
-    #     """:returns: list of master ip addresses"""
-    #     if "ipOffsets" in self._cluster_info:
-    #         offset = self._cluster_info['ipOffsets']['masters']
-    #     else:
-    #         offset = self._master_ip_offset
-    #     return [
-    #         str(
-    #             ipaddress.IPv4Network(self._env_info['externalNet'])[i + offset])
-    #         for i in range(self._env_info['masters']['nodes'])
-    #     ]
-    #
-    # def get_initial_cluster(self):
-    #     """:returns: - string - etcd initial cluster string"""
-    #     masters = self.get_master_hosts()
-    #     return ",".join([
-    #         "%s=http://%s:2380" % (master, master)
-    #         for index, master in enumerate(masters)
-    #     ])
-    #
-    # def get_etcd_hosts(self):
-    #     """:returns: string - comma delimeted string of proto://host:port"""
-    #     return ",".join(
-    #         ["http://%s:2379" % master for master in self.get_master_hosts()])
-
-    # def update_kube_config(self):
-    #     """ Add new context into kube config"""
-    #     update = 'n'
-    #     update = input("Auto update kube config? (y|n)")
-    #     if update == 'y':
-    #         if os.path.isfile(
-    #                 "{}/.kube/config".format(os.path.expanduser("~"))):
-    #             shutil.copy(
-    #                 "{}/.kube/config".format(os.path.expanduser("~")),
-    #                 "{}/.kube/config.bak".format(os.path.expanduser("~")))
-    #         kubectl(
-    #             "config", "set-cluster", "{}-{}-cluster".format(
-    #                 self._cluster_info['cluster_env'],
-    #                 self._cluster_info['cluster_id']),
-    #             "--server=https://{}:{}".format(
-    #                 self._cluster_info["kube_master"],
-    #                 self._cluster_info["master_port"]),
-    #             "--certificate-authority={}/ssl/ca.pem".format(self._cluster_info['buttdir']))
-    #         kubectl(
-    #             "config", "set-credentials", "{}-{}-admin".format(
-    #                 self._cluster_info['cluster_env'],
-    #                 self._cluster_info['cluster_id']),
-    #             "--certificate-authority={}/ssl/ca.pem".format(self._cluster_info['buttdir']),
-    #             "--client-key={}/ssl/admin-key.pem".format(self._cluster_info['buttdir']),
-    #             "--client-certificate={}/ssl/admin.pem".format(self._cluster_info['buttdir']))
-    #         kubectl("config", "set-context", "{}-{}-system".format(
-    #             self._cluster_info['cluster_env'],
-    #             self._cluster_info['cluster_id']),
-    #                 "--cluster={}-{}-cluster".format(
-    #                     self._cluster_info['cluster_env'],
-    #                     self._cluster_info['cluster_id']),
-    #                 "--user={}-{}-admin".format(
-    #                     self._cluster_info['cluster_env'],
-    #                     self._cluster_info['cluster_id']))
-    #         kubectl("config", "use-context", "{}-{}-system".format(
-    #             self._cluster_info['cluster_env'],
-    #             self._cluster_info['cluster_id']))
-
-    # def get_worker_ip(self, index):
-    #     """:returns: string - ip in externalNet range"""
-    #     return str(
-    #         ipaddress.IPv4Network(self._env_info['externalNet'])[
-    #             index + self._worker_ip_offset])
-
-    # def get_kube_addons(self):
-    #     """:returns: string - everything in butt-templates/addons converted to string"""
-    #     addons = []
-    #     for addon in glob.glob("butt-templates/addons/*.yaml"):
-    #         with open(addon, 'r') as file:
-    #             temp = file.read()
-    #         if re.search("dashboard-ingress", addon):
-    #             temp = temp.format(dashboardFQDN=self._cluster_info['dashboardFQDN'])
-    #         addons.append({
-    #             "path":
-    #             "/etc/kubernetes/addons/%s" % (os.path.basename(addon)),
-    #             "content":
-    #             temp,
-    #             "permissions":
-    #             "0644",
-    #             "owner":
-    #             "root"
-    #         })
-    #     return addons
-
-    # @staticmethod
-    # def get_kube_manifests():
-    #     """:returns: dict of strings - everything in butt-templates/manifets/* converted to string"""
-    #     manifests = {'master': [], 'worker': []}
-    #     for name in ['master', 'worker']:
-    #         for manifest in glob.glob(
-    #                 "butt-templates/manifests/%s/*.yaml" % name):
-    #             with open(manifest, 'r') as file:
-    #                 temp = file.read()
-    #             manifests[name].append({
-    #                 "path":
-    #                 "/etc/kubernetes/manifests/%s" %
-    #                 (os.path.basename(manifest)),
-    #                 "content":
-    #                 temp,
-    #                 "permissions":
-    #                 "0644",
-    #                 "owner":
-    #                 "root"
-    #             })
-    #     return manifests
